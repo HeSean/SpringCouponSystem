fc491c69af44f4177ad51443cda60cf51fa818fe
Sean <hedsean@gmail.com>
2019-09-07 01:44:51 +0300

    fetching coupons working:D

--- src/main/java/com/seanhed/data/repo/CouponRepository.java
+++ src/main/java/com/seanhed/data/repo/CouponRepository.java
@@ -4,10 +4,11 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.seanhed.beans.Coupon;
 import com.seanhed.beans.CouponType;
-
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
 	public Collection<Coupon> deleteByName(String name);
--- src/main/java/com/seanhed/data/rest/CouponController.java
+++ src/main/java/com/seanhed/data/rest/CouponController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.seanhed.data.service.CouponService;
 
 @RestController
 @RequestMapping("/coupon/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CouponController {
 
 	@Autowired
@@ -39,6 +41,7 @@ public class CouponController {
 	// http://localhost:8080/coupon/getAll
 	@GetMapping("/getAll")
 	public List<Coupon> getCoupons() {
+		
 		return service.getCoupons();
 	}
 
